c6588503d0b31e44ad1ea30ed2328a1702a343d3
Dakota Chatt <dvchatt@gmail.com>
2022-03-27 10:02:41 -0700

    Add ability to make multiple guesses, add auto select next text field after character typed, add enable current guess line (other lines disabled)

--- WordleClone/Controller/ViewController.swift
+++ WordleClone/Controller/ViewController.swift
@@ -11,8 +11,8 @@ class ViewController: UIViewController {
     
     let testWord : [String] = ["P", "I", "D", "G", "E"]
     var guessNum = 1
-    var testWordArray : [String] = []
-    var userGuess : [String] = []
+    var currentGuessTextFieldCollection : [UITextField] = []
+    var userGuess : [String] = ["", "", "", "", ""]
     
     @IBOutlet var guess1TextFields: [UITextField]!
     @IBOutlet var guess2TextFields: [UITextField]!
@@ -32,49 +32,113 @@ class ViewController: UIViewController {
             self.guess5TextFields[i].delegate = self
             self.guess6TextFields[i].delegate = self
         }
+        
+        currentGuessTextFieldCollection = guess1TextFields
+        guess1TextFields[0].becomeFirstResponder()
     }
-    
     
     func checkAnswer() {
         for i in 0...4 {
-            userGuess.append(guess1TextFields[i].text!)
-        }
-        
-        if(guessNum == 1) {
-            for i in 0...4 {
-               var presentInWord = false
-                
-                if(userGuess[i] == testWord[i]) {
-                    guess1TextFields[i].backgroundColor = UIColor.green
-                } else {
-                    for j in 0...4 {
-                        if(i != j) {
-                            if(userGuess[i] == testWord[j]) {
-                                guess1TextFields[i].backgroundColor = UIColor.yellow
-                                presentInWord = true
-                            }
+           var presentInWord = false
+            
+            if(userGuess[i] == testWord[i]) {
+                currentGuessTextFieldCollection[i].backgroundColor = UIColor.green
+            } else {
+                for j in 0...4 {
+                    if(i != j) {
+                        if(userGuess[i] == testWord[j]) {
+                            currentGuessTextFieldCollection[i].backgroundColor = UIColor.yellow
+                            presentInWord = true
                         }
                     }
-                    
-                    if(!presentInWord) {
-                        guess1TextFields[i].backgroundColor = UIColor.gray
-                    }
+                }
+                
+                if(!presentInWord) {
+                    currentGuessTextFieldCollection[i].backgroundColor = UIColor.gray
                 }
             }
         }
+        
+        //Used for startNextGuess() to determine which line of textFields to enable
+        guessNum += 1
+        startNextGuess()
     }
+    
+    func startNextGuess() {
+        print("Should start next guess")
+        switch(guessNum) {
+        case 2:
+            for i in 0...4 {
+                guess1TextFields[i].isEnabled = false
+                guess2TextFields[i].isEnabled = true
+                currentGuessTextFieldCollection = guess2TextFields
+                guess2TextFields[0].becomeFirstResponder()
+            }
+            break
+        case 3:
+            for i in 0...4 {
+                guess2TextFields[i].isEnabled = false
+                guess3TextFields[i].isEnabled = true
+                currentGuessTextFieldCollection = guess3TextFields
+                guess3TextFields[0].becomeFirstResponder()
+            }
+            break
+        case 4:
+            for i in 0...4 {
+                guess3TextFields[i].isEnabled = false
+                guess4TextFields[i].isEnabled = true
+                currentGuessTextFieldCollection = guess4TextFields
+                guess4TextFields[0].becomeFirstResponder()
+            }
+            break
+        case 5:
+            for i in 0...4 {
+                guess4TextFields[i].isEnabled = false
+                guess5TextFields[i].isEnabled = true
+                currentGuessTextFieldCollection = guess5TextFields
+                guess5TextFields[0].becomeFirstResponder()
+            }
+            break
+        case 6:
+            for i in 0...4 {
+                guess5TextFields[i].isEnabled = false
+                guess6TextFields[i].isEnabled = true
+                currentGuessTextFieldCollection = guess6TextFields
+                guess6TextFields[0].becomeFirstResponder()
+            }
+            break
+        default:
+            break
+        }
+    }
+    
+    //Auto select next text field after user types a letter
+    @IBAction func letterChanged(_ sender: UITextField) {
+        if(sender.text != "") {
+            let currentTextFieldTag = sender.tag
+            
+            //Formula below ensures when tag is > length of word, resulting number is still between 0 - 4
+            print((sender.tag - (5 * (guessNum - 1))))
+            userGuess[(sender.tag - (5 * (guessNum - 1)))] = sender.text!
+            
+            if let nextTextField = self.view.viewWithTag(currentTextFieldTag + 1) as? UITextField {
+                nextTextField.becomeFirstResponder()
+            }
+        }
+    }
+    
 }
 
 //MARK: - Keyboard dismissing methods
 extension ViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        
         checkAnswer()
-        
-        self.view.endEditing(true)
+        print(guessNum)
+        print(userGuess)
         return false
     }
     
+    //Ensures each text field can only have a single character
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         let currentText = textField.text ?? ""
         guard let stringRange = Range(range, in: currentText) else { return false }
